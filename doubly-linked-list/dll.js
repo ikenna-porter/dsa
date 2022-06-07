83f99cfc3608f751d066a171ab9b8485a18b4daf
Ikenna <ikenna.porter@gmail.com>
2022-06-07 21:29:38 +0200

    Add set and get methods

--- doubly-linked-list/dll.js
+++ doubly-linked-list/dll.js
@@ -23,12 +23,86 @@ class DoublyLinkedList {
 
     pop() {
         if (this.length === 0) return undefined;
+        const poppedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
 
-        this.tail = this.tail.prev;
-        this.tail.next = undefined;
         this.length--;
+        return poppedNode;
     }
 
+    shift() {
+        if (this.length === 0) return undefined;
+        const removedNode = this.head;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = removedNode.next;
+            removedNode.next = null;
+            this.head.prev = null;
+        }
+        this.length--;
+        return removedNode;
+    }
+
+    unshift(value) {
+        const newNode = new Node(value);
+        const oldHead = this.head;
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head = newNode;
+            newNode.next = oldHead;
+            oldHead.prev = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index > this.length) return undefined;
+
+        if (this.index >= this.length / 2) {
+            let counter = this.length - 1;
+            let current = this.tail;
+            let prev = this.tail.prev;
+
+            while (counter > index) {
+                current = prev;
+                prev = current.prev;
+                counter--;
+            }
+        } else {
+            let counter = 0;
+            let current = this.head;
+            let next = this.head.next;
+
+            while (counter < index) {
+                current = next;
+                next = current.next;
+                counter++;
+            }
+        }
+        return current;
+    }
+
+    set(index, value) {
+        const node = this.get(index);
+        if (node) {
+            node.value = value;
+            return true;
+        }
+        return false;
+    }
 }
 
 class Node {
